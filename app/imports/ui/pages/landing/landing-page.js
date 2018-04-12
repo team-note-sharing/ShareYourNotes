581b978d6dcf595e719df1945833224d42240a52
jonathanmng <31453488+jonathanMNg@users.noreply.github.com>
2018-04-11 18:35:09 -1000

    update alnding page and header

--- app/imports/ui/pages/landing/landing-page.js
+++ app/imports/ui/pages/landing/landing-page.js
@@ -1,16 +1,10 @@
 import { Template } from 'meteor/templating';
-import { FlowRouter } from 'meteor/kadira:flow-router';
 import { $ } from 'meteor/jquery';
 
-Template.User_Header.onRendered(function() {
+Template.Landing_Page.onRendered(function() {
   this.$('.ui.sidebar').sidebar({
     context: $('.ui.pushable.segment'),
     transition: 'overlay'
   }).sidebar('attach events', '#mobile_item');
 });
 
-Template.User_Header.helpers({
-  routeUserName() {
-    return FlowRouter.getParam('username');
-  },
-});
