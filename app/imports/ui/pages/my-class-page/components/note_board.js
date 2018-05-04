1263c01d2c96427d02f37afcbba569aabb664f48
jonathanmng <31453488+jonathanMNg@users.noreply.github.com>
2018-05-03 21:45:46 -1000

    update my note page

--- app/imports/ui/pages/my-class-page/components/note_board.js
+++ app/imports/ui/pages/my-class-page/components/note_board.js
@@ -33,6 +33,70 @@ export default class NoteBoard extends Component {
               </div>
             </div>
           </div>
+          <div className="ui comments">
+            <h3 className="ui dividing header">Comments</h3>
+            <div className="comment">
+              <a className="avatar">
+                <img src="/images/baek.jpg"/>
+              </a>
+              <div className="content">
+                <a className="author">Baek</a>
+                <div className="metadata">
+                  <span className="date">Today at 5:42PM</span>
+                </div>
+                <div className="text">
+                  How artistic!
+                </div>
+                <div className="actions">
+                  <a className="reply">Reply</a>
+                </div>
+              </div>
+            </div>
+            <div className="comment">
+              <a className="avatar">
+                <img src="/images/binsted.jpg"/>
+              </a>
+              <div className="content">
+                <a className="author">Kim</a>
+                <div className="metadata">
+                  <span className="date">Yesterday at 12:30AM</span>
+                </div>
+                <div className="text">
+                  <p>This has been very useful for my research. Thanks as well!</p>
+                </div>
+                <div className="actions">
+                  <a className="reply">Reply</a>
+                </div>
+              </div>
+              <div className="comments">
+                <div className="comment">
+                  <a className="avatar">
+                    <img src="/images/johnson.jpg"/>
+                  </a>
+                  <div className="content">
+                    <a className="author">Philip</a>
+                    <div className="metadata">
+                      <span className="date">Just now</span>
+                    </div>
+                    <div className="text">
+                      Elliot you are always so right :)
+                    </div>
+                    <div className="actions">
+                      <a className="reply">Reply</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <form className="ui reply form">
+              <div className="field">
+                <textarea></textarea>
+              </div>
+              <div className="ui blue labeled submit icon button">
+                <i className="icon edit"></i> Add Reply
+              </div>
+            </form>
+          </div>
         </div>
     );
   }
